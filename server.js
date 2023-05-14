13fab91de1eb4d7b4537ff4a2f61241fbe85ad30
mrviularry <91667248+mrviularry@users.noreply.github.com>
2023-05-14 18:42:58 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const uuid = require('uuid');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+let licenses = [];
+
+app.get('/generate', (req, res) => {
+    const newKey = uuid.v4();
+    licenses.push(newKey);
+    res.json(newKey);
+});
+
+app.get('/verify/:key', (req, res) => {
+    const key = req.params.key;
+    const valid = licenses.includes(key);
+    res.json({ valid });
+});
+
+
+app.get('/delete/:key', (req, res) => {
+    const key = req.params.key;
+    const index = licenses.indexOf(key);
+    if (index > -1) {
+        licenses.splice(index, 1);
+        res.json({ message: 'Deleted successfully' });
+    } else {
+        res.json({ message: 'Key not found' });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
